3c557de52488bc4c3939b5dca9567a50fd05f21b
gcgean <81776909+gcgean@users.noreply.github.com>
2026-02-22 15:28:40 -0300

    zz

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,8 @@ export default defineConfig({
     tsconfigPaths(),
   ],
   server: {
+    port: 5174,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
